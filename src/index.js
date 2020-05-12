3bd9dfdccf1a97f96e7e62bad6797a2823fb583c
karlfleener <karlfleener@gmail.com>
2020-05-12 17:17:50 -0400

    add quicksort function

--- src/index.js
+++ src/index.js
@@ -72,24 +72,60 @@ class SortingVisualizer {
     this.shuffleArray();
     this.drawCanvas();
   }
+
+  algorithm() {
+    let algorithmInput = document.getElementById('algorithm').value;
+
+    if (algorithmInput === "Bubble Sort") {
+      return this.bubbleSort(this.array)
+    }
+    if (algorithmInput === "Merge Sort") {
+      return this.mergeSort(this.array)
+    }
+    if (algorithmInput === "Quick Sort") {
+      return this.quickSort(this.array, 0, this.array.size)
+    }
+    if (algorithmInput === "") return
+  }
+
+  isSorted(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        return false
+      }
+    }
+    return true;
+  }
+
+
   
   sort() {
-    let sortArray = this.bubbleSort(this.array);
+    let sortArray = this.algorithm();
+    if (sortArray === undefined) return;
 
     const animate = () => {
-      
       let speed = Math.floor(1000 / Number(document.getElementById("speed").value))
-        setTimeout(function() { 
-        requestAnimationFrame(animate); 
-      }, speed);
-      
-      this.drawCanvas();
-      sortArray.next(); // call next iteration of the bubbleSort function
+        // setTimeout(animate, speed);
+        // requestAnimationFrame(animate); 
+
+        this.drawCanvas();
+
+        if (this.isSorted(this.array)) {
+          return;
+        } else {
+          setTimeout(animate, speed);
+          sortArray.next();
+        };
+      // console.log(this.isSorted(this.array))
+      // sortArray.next() // call next iteration of the bubbleSort function
     };
+
     animate();
   }
-  
-  * bubbleSort(arr) {
+
+//---------------------------------Algorithms-----------------------------------
+ 
+* bubbleSort(arr) {
     let swapped = true;
     while(swapped) {
       swapped = false;
@@ -97,7 +133,7 @@ class SortingVisualizer {
         if (arr[i] > arr[i + 1]) {
           this.swap(arr, i, i + 1);
           swapped = true;
-          yield swapped; // pause here
+          yield swapped // pause here
         }
       }
     }
@@ -109,7 +145,59 @@ class SortingVisualizer {
     arr[idx1] = arr[idx2];
     arr[idx2] = temp;
   }
+
+  * quickSort(arr, min, max = arr.length) {
+  if (max - min <= 1) return arr; // base case
+
+  // partitioning
+  let [pivot, less, greater] = [arr[min], [], []];
+  for (let i = min + 1; i < max; i++) {
+    if (arr[i] < pivot) less.push(arr[i]);
+    else greater.push(arr[i]);
+    arr.splice(min, i - min + 1, ...less.concat(pivot, greater));
+    yield arr;
+  }
+
+  yield* this.quickSort(arr, min, min + less.length);
+  yield* this.quickSort(arr, min + less.length + 1, max);
+}
   
+  * mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let midIdx = Math.floor(arr.length / 2);
+  let left = arr.slice(0, midIdx);
+  let right = arr.slice(midIdx);
+  
+  yield [...left, midIdx, ...right];
+
+  let sortedLeft = this.mergeSort(left);
+  let sortedRight = this.mergeSort(right);
+
+  this.merge(sortedLeft, sortedRight);
+}
+
+
+merge(arr1, arr2) {
+  let merged = [];
+
+  while (arr1.length || arr2.length) {
+    let ele1 = arr1.length ? arr1[0] : Infinity;
+    let ele2 = arr2.length ? arr2[0] : Infinity;
+
+    let next;
+    if (ele1 < ele2) {
+      next = arr1.shift();
+    } else {
+      next = arr2.shift();
+    }
+
+    merged.push(next);
+  }
+
+  return merged;
+}
+
 }
 
 //----------------------------------Run Program---------------------------------
