31bbcdc605f45e9c64a14c3f953d42856f297b78
karlfleener <karlfleener@gmail.com>
2020-05-21 09:11:47 -0400

    updat meta tags

--- src/index.js
+++ src/index.js
@@ -161,68 +161,40 @@ class SortingVisualizer {
 
   //------------------------------------------------------------------------------
 
-  //   *mergeSort(arr) {
-  //   if (arr.length <= 1) return arr;
+//   mergeSort(arr) {
+//   if (arr.length <= 1) return arr;
+
+//   let midIdx = Math.floor(arr.length / 2);
+//   let left = arr.slice(0, midIdx);
+//   let right = arr.slice(midIdx);
+
+//   let sortedLeft = mergeSort(left);
+//   let sortedRight = mergeSort(right);
+
+//   return merge(sortedLeft, sortedRight);
+// }
 
 
-  //   let midIdx = Math.floor(arr.length / 2);
-  //   let left = arr.slice(0, midIdx);
-  //   let right = arr.slice(midIdx);
+// merge(arr1, arr2) {
+//   let merged = [];
 
+//   while (arr1.length || arr2.length) {
+//     let ele1 = arr1.length ? arr1[0] : Infinity;
+//     let ele2 = arr2.length ? arr2[0] : Infinity;
 
-  //   let sortedLeft = this.mergeSort(left);
-  //   let sortedRight = this.mergeSort(right);
+//     let next;
+//     if (ele1 < ele2) {
+//       next = arr1.shift();
+//     } else {
+//       next = arr2.shift();
+//     }
 
+//     merged.push(next);
+//   }
 
+//   return merged;
+// }
 
-  //   function *merge(sortedLeft, sortedRight) {
-
-  //     // console.log(arr)
-
-  //     arr = [];
-
-  //     while (sortedLeft.length || sortedRight.length) {
-  //       let ele1 = sortedLeft.length ? sortedLeft[0] : Infinity;
-  //       let ele2 = sortedRight.length ? sortedRight[0] : Infinity;
-
-  //       let next;
-  //       if (ele1 < ele2) {
-  //         next = sortedLeft.shift();
-  //       } else {
-  //         next = sortedRight.shift();
-  //       }
-
-  //       arr.push(next);
-  //     }
-
-  //     console.log(arr)
-  //     yield* arr;
-  //   }
-  //   // console.log(arr);
-
-  //   yield* merge(sortedLeft, sortedRight);
-  // }
-
-  // merge(arr1, arr2) {
-  //   let merged = [];
-
-  //   while (arr1.length || arr2.length) {
-  //     let ele1 = arr1.length ? arr1[0] : Infinity;
-  //     let ele2 = arr2.length ? arr2[0] : Infinity;
-
-  //     let next;
-  //     if (ele1 < ele2) {
-  //       next = arr1.shift();
-  //     } else {
-  //       next = arr2.shift();
-  //     }
-
-  //     merged.push(next);
-  //     // yield* merged
-  //   }
-  //   return merged;
-
-  // }
 }
 
 //----------------------------------Run Program---------------------------------
